e7973de33cb57a96bc61667b1b5585825edb5fab
maulana-tech <firdaussyah03@gmail.com>
2026-08-05 21:18:02 +0700

    delete(web/src/lib/save.ts): auto update

--- web/src/lib/save.ts
+++
@@ -1,7 +0,0 @@
-import { saveEvm } from '@/lib/save.evm'
-import { saveMock } from '@/lib/save.mock'
-import { CONTRACT_ID } from '@/lib/config'
-import type { SaveService } from '@/lib/types'
-
-export const save: SaveService = CONTRACT_ID === '' ? saveMock : saveEvm
-
